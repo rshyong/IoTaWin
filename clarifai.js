c154df689d8e45b928706b8dd2f41b6bfe409271
Richard Shyong <rshyong@gmail.com>
2017-03-25 12:58:31 -0400

    adding model

--- clarifai.js
+++ clarifai.js
@@ -7,11 +7,78 @@ var app = new Clarifai.App(
 );
 
 // predict the contents of an image by passing in a url
-app.models.predict(Clarifai.GENERAL_MODEL, 'http://pngimg.com/uploads/beer/beer_PNG2390.png').then(
-function(response) {
-    console.log(response.outputs[0].data);
-},
-function(err) {
-    console.error(err);
-}
+// app.models.predict(Clarifai.GENERAL_MODEL, 'http://pngimg.com/uploads/beer/beer_PNG2390.png').then(
+// function(response) {
+//     console.log(response.outputs[0].data);
+// },
+// function(err) {
+//     console.error(err);
+// }
+// );
+
+app.inputs.create({
+    url: "https://i.imgur.com/13q6O02.jpg",
+    concepts: [
+        {
+        id: "bad",
+        value: true
+        }
+    ]
+    },
+    {
+    url: "https://i.imgur.com/V575J1P.jpg",
+    concepts: [
+        {
+        id: "bad",
+        value: true
+        }
+    ]
+    },
+    {
+    url: "https://i.imgur.com/uyYKKQK.jpg",
+    concepts: [
+        {
+        id: "bad",
+        value: true
+        }
+    ]
+    },
+    {
+    url: "https://i.imgur.com/V575J1P.jpg",
+    concepts: [
+        {
+        id: "bad",
+        value: true
+        }
+    ]
+    },
+    {
+    url: "https://i.imgur.com/Q9u8tx8.jpg",
+    concepts: [
+        {
+        id: "good",
+        value: true
+        }
+    ]
+    }).then(
+    function(response) {
+        console.log(response);
+    },
+    function(err) {
+        console.log(err);
+    }
+);
+
+app.models.create(
+    "good",
+    [
+        { "id": "good" }
+    ]
+    ).then(
+    function(response) {
+        console.log(response);
+    },
+    function(err) {
+        console.log(err);
+    }
 );
